99ea0dabc449535d00e0ed4cc8604bb1ed82fa4e
sjumilli <sjur.millidahl@finn.no>
2016-01-18 09:10:22 +0100

    Sequence a List<Try> to a Try<List>

--- src/test/groovy/no/finn/lambdacompanion/TrySpec.groovy
+++ src/test/groovy/no/finn/lambdacompanion/TrySpec.groovy
@@ -4,34 +4,34 @@ import spock.lang.Specification
 
 class TrySpec extends Specification {
 
-    def "If Try is a Success, a successful map should yield new success" () {
+    def "If Try is a Success, a successful map should yield new success"() {
         given:
         def myTry = new Success<>("3")
         when:
-        def actual = myTry.map({String s -> Integer.parseInt(s)})
+        def actual = myTry.map({ String s -> Integer.parseInt(s) })
         then:
         actual instanceof Success<Integer>
     }
 
-    def "If Try is a Success, a failing map should yield failure" () {
+    def "If Try is a Success, a failing map should yield failure"() {
         given:
         def myTry = new Success<>("blargh")
         when:
-        def actual = myTry.map({String s -> Integer.parseInt(s)})
+        def actual = myTry.map({ String s -> Integer.parseInt(s) })
         then:
         actual instanceof Failure
     }
 
-    def "If Try is a Success, a failing flatMap should yield failure" () {
+    def "If Try is a Success, a failing flatMap should yield failure"() {
         given:
         def myTry = new Success<>("blargh")
         when:
-        def actual = myTry.flatMap({s -> new Failure<>(new RuntimeException("asdf"))})
+        def actual = myTry.flatMap({ s -> new Failure<>(new RuntimeException("asdf")) })
         then:
         actual instanceof Failure
     }
 
-    def "if Try is a Success, a oElse should get the value, not the fallback value" () {
+    def "if Try is a Success, a oElse should get the value, not the fallback value"() {
         given:
         def myTry = new Success<>("blargh")
         when:
@@ -40,7 +40,7 @@ class TrySpec extends Specification {
         actual == "blargh"
     }
 
-    def "if Try is a Failure, a orElse should get the fallback value" () {
+    def "if Try is a Failure, a orElse should get the fallback value"() {
         given:
         def myTry = new Failure<>(new RuntimeException("Asdf"))
         when:
@@ -49,25 +49,25 @@ class TrySpec extends Specification {
         actual == "somethingElse"
     }
 
-    def "if Try is a Success, a orElseGet should get the success value" () {
+    def "if Try is a Success, a orElseGet should get the success value"() {
         given:
         def myTry = new Success<>("blargh")
         when:
-        def actual = myTry.orElseGet({ -> "roargh"})
+        def actual = myTry.orElseGet({ -> "roargh" })
         then:
         actual == "blargh"
     }
 
-    def "if Try is a Failure, a orElseGet should get a fallback supplier value" () {
+    def "if Try is a Failure, a orElseGet should get a fallback supplier value"() {
         given:
         def myTry = new Failure<>(new IllegalArgumentException("Nooot good"))
         when:
-        def actual = myTry.orElseGet({-> "roargh"})
+        def actual = myTry.orElseGet({ -> "roargh" })
         then:
         actual == "roargh"
     }
 
-    def "if Try is a Success, toOptional should give an Optional around the value" () {
+    def "if Try is a Success, toOptional should give an Optional around the value"() {
         given:
         def myTry = new Success<>("yoyo")
         when:
@@ -76,7 +76,7 @@ class TrySpec extends Specification {
         actual.get() == "yoyo"
     }
 
-    def "if Try is a Failure, toOptional should give empty Optional" () {
+    def "if Try is a Failure, toOptional should give empty Optional"() {
         given:
         def myTry = new Failure<>(new Exception(":x"))
         when:
@@ -85,108 +85,108 @@ class TrySpec extends Specification {
         !actual.isPresent()
     }
 
-    def "if Try is a Success, recover should run the success function" () {
+    def "if Try is a Success, recover should run the success function"() {
         given:
         def myTry = new Success<>("hello")
         when:
-        def actual = myTry.recover({s -> s + " world"},
-                {e -> "aaw, that's ok, world."})
+        def actual = myTry.recover({ s -> s + " world" },
+                { e -> "aaw, that's ok, world." })
         then:
         actual == "hello world"
     }
 
-    def "If Try is a Failure, fold should give a fallback Success" () {
+    def "If Try is a Failure, fold should give a fallback Success"() {
         given:
         def myTry = new Failure<>(new RuntimeException(":("))
         when:
-        def actual = myTry.recover({s -> " world"},
-                {e -> "aaw, that's ok, world."})
+        def actual = myTry.recover({ s -> " world" },
+                { e -> "aaw, that's ok, world." })
         then:
         actual == "aaw, that's ok, world."
     }
 
-    def "If Try is a Success, toEither should give an Either.right around the value" () {
+    def "If Try is a Success, toEither should give an Either.right around the value"() {
         given:
         def myTry = new Failure<>(new ConcurrentModificationException(":o"))
         when:
-        def actual = myTry.toEither().fold({i -> i}, {i -> i})
+        def actual = myTry.toEither().fold({ i -> i }, { i -> i })
         then:
         actual instanceof ConcurrentModificationException
     }
 
-    def "peek should return a Try unchanged" () {
+    def "peek should return a Try unchanged"() {
         given:
         def myTry = new Failure(new ArrayIndexOutOfBoundsException(":o"))
         when:
-        def actual = myTry.peek({f -> System.out.println("something")})
+        def actual = myTry.peek({ f -> System.out.println("something") })
         then:
-        actual.recover({s -> s}, {f -> f instanceof ArrayIndexOutOfBoundsException? 1 : 0}) == 1
+        actual.recover({ s -> s }, { f -> f instanceof ArrayIndexOutOfBoundsException ? 1 : 0 }) == 1
     }
 
-    def "peekFailure should return a Try unchanged" () {
+    def "peekFailure should return a Try unchanged"() {
         given:
         def myTry = new Failure(new ArrayIndexOutOfBoundsException(":o"))
         when:
-        def actual = myTry.peekFailure({f -> System.out.println("something")})
+        def actual = myTry.peekFailure({ f -> System.out.println("something") })
         then:
-        actual.recover({s -> s}, {f -> f instanceof ArrayIndexOutOfBoundsException? 1 : 0}) == 1
+        actual.recover({ s -> s }, { f -> f instanceof ArrayIndexOutOfBoundsException ? 1 : 0 }) == 1
     }
 
-    def "should create a Try from a ThrowingFunction" () {
+    def "should create a Try from a ThrowingFunction"() {
         given:
-        def myTry = Try.of({i -> new Integer(i)}, 3);
+        def myTry = Try.of({ i -> new Integer(i) }, 3);
         when:
         def actual = myTry
         then:
         actual instanceof Try
     }
 
-    def "should create a Try from a ThrowingBiFunction" () {
+    def "should create a Try from a ThrowingBiFunction"() {
         given:
-        def myTry = Try.of({a,b -> a/b}, 1, 0)
+        def myTry = Try.of({ a, b -> a / b }, 1, 0)
         when:
         def actual = myTry
         then:
         actual instanceof Try
     }
 
-    def "should create a Try from a ThrowingSupplier" () {
+    def "should create a Try from a ThrowingSupplier"() {
         given:
-        def myTry = Try.of({ -> new String("")})
+        def myTry = Try.of({ -> new String("") })
         when:
         def actual = myTry
         then:
         actual instanceof Try
     }
 
-    def "should get value from orElseThrow if success" () {
+    def "should get value from orElseThrow if success"() {
         given:
-        def myTry = Try.of({ -> new String("fish")})
+        def myTry = Try.of({ -> new String("fish") })
         when:
-        def actual = myTry.orElseThrow({e -> e})
+        def actual = myTry.orElseThrow({ e -> e })
         then:
         actual == "fish"
     }
 
-    def "should throw exception mapped through provided function when orElseThrow on a Failure" () {
+    def "should throw exception mapped through provided function when orElseThrow on a Failure"() {
         given:
         def myTry = new Failure(new IOException("floppy drive way too busy"))
         when:
-        myTry.orElseThrow({e -> e})
+        myTry.orElseThrow({ e -> e })
         then:
         thrown(IOException)
     }
 
-    def "should get value from orElseRethrow if success" () {
+    def "should get value from orElseRethrow if success"() {
         given:
-        def myTry = Try.of({ -> new String("fish")})
+        def myTry = Try.of({ -> new String("fish") })
         when:
         def actual = myTry.orElseRethrow()
         then:
         actual == "fish"
     }
 
-    def "should throw caught exception when orElseRethrow on a Failure" () {
+    def "should throw caught exception when orElseRethrow on a Failure"() {
         given:
         def myTry = new Failure(new IOException("floppy drive way too busy"))
         when:
@@ -194,6 +194,36 @@ class TrySpec extends Specification {
         then:
         thrown(IOException)
     }
+
+    def "should sequence a list of successes to a try of list" () {
+        given:
+        def successes = Arrays.asList(new Success<>("yo"), new Success<>("dude"), new Success<>("hmm"))
+        when:
+        def actual = Try.sequence(successes).orElse(Collections.emptyList())
+        then:
+        actual.get(0) == "yo"
+        actual.get(1) == "dude"
+        actual.get(2) == "hmm"
+    }
+
+    def "should sequence a list containing a failure to first failure in list" () {
+        given:
+        def tries = Arrays.asList(new Success<>("yo"), new Failure<>(new IllegalFormatCodePointException(3)))
+        when:
+        Try.sequence(tries).orElseRethrow()
+        then:
+        thrown(IllegalFormatCodePointException)
+    }
+
+    def "should give illegalargument failure on sequencing empty list" () {
+        given:
+        def tries = Collections.emptyList()
+        when:
+        Try.sequence(tries).orElseRethrow()
+        then:
+        thrown(IllegalArgumentException)
+    }
+
 
 
 }
